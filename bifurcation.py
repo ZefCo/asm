3f5cede33dd01fc5ec14276fcd8137ebd32e3098
ZefCo <zefram.cochrane2024@gmail.com>
2021-09-12 11:34:20 -0500

    Finished Bifurcation

--- bifurcation.py
+++ bifurcation.py
@@ -7,75 +7,106 @@ import math
 
 
 def main():
-    root = platformer()
     cwd = os.getcwd()
-    ffig = os.path.join(cwd, 'f_a.png')
-    sfig = os.path.join(cwd, 'sin_a.png')
 
-    scale = 10000
-    dotsize = 0.1
-    jumps = 1
+    func1 = fofa
+    a1: int = 4
+    path1 = os.path.join(cwd, 'f_fig.png')
 
-    amax = 4 * scale
-    amin = 1
+    func2 = sofa
+    a2: int = 1
+    path2 = os.path.join(cwd, 'sin_fig.png')
     
-    aaxis = []
-    faxis = []
-    saxis = []
+    bifur(a1, func1, close=False)
+    bifur(a1, func1, close=False)
+    bifur(a1, func1, close=False)
+    bifur(a1, func1, close=False)
+    bifur(a1, func1, close=False)
+    bifur(a1, func1, path=path1)
+
+    bifur(a2, func2, close=False)
+    bifur(a2, func2, close=False)
+    bifur(a2, func2, close=False)
+    bifur(a2, func2, close=False)
+    bifur(a2, func2, close=False)
+    bifur(a2, func2, path=path2)
+
+
+def bifur(amax: int, 
+          func, 
+          scale=10000, 
+          dotsize=0.1, 
+          jumps=1, 
+          path=None,
+          show=False,
+          close=True):
+    '''
+    Requires a maximum value for a (pick a to be normalized beforehand), and
+    an input fucntion (func). Outputs a bifurcation plot of the input function.
+    X is assumed to be normalized and will be chosen at random from between
+    0 and 1 using random.random()
+
+    The plot will be saved to a file path if the filepath is input as a string,
+    else the plot will be shown for manual saving later.
+    '''
+    cwd = os.getcwd()
+    fig = os.path.join(cwd, 'f_a.png')
+
+    amax: int = amax * scale
+    amin: int = 1
+    
+    aaxis: list = []
+    faxis: list = []
     for i in range(amin, amax, jumps):
-        iters = 100
+        iters: int = 100
         
-        a = i/scale
+        a: float = i/scale
         aaxis.append(a)
 
         x: float = jitter()
 
-        f: float = fofa(a, x, iters)
+        f, title = func(a, x, iters)
         faxis.append(f)
-
-        s: float = sofa(a, x, iters)
-        saxis.append(s)
     
     plt.scatter(aaxis, faxis, s=dotsize)
     plt.xlabel('a')
-    plt.title('f(x) = a x (1-x)')
-    plt.savefig(ffig)
+    plt.title(title)
+    plt.savefig(fig)
 
-    plt.close()
+    if isinstance(path, str):
+        plt.savefig(path)
+    
+    if show:
+        plt.show()
 
-    plt.scatter(aaxis, saxis, s=dotsize)
-    plt.xlabel('a')
-    plt.title('f(x) = a sin(pi x)')
-    plt.show(sfig)
+    if close:
+        plt.close()
 
 
 def jitter(scale=1):
-    sign = random.choice(['+', '-'])
-
-    j = random.random()
-    j = j / scale
-    j = round(j, 3)
-
-    # if sign in '-':
-    #     j = -j
+    j: float = random.random()
+    j: float = j / scale
+    j: float = round(j, 3)
 
     return j
 
 
 def fofa(a: float, x0: float, n: int):
-    x = x0
+    x: float = x0
+    title: str = 'f(x) = a x (1 - x)'
     for _ in range(n):
         x = a*x*(1-x)
 
-    return x
+    return x, title
 
 
 def sofa(a: float, x0: float, n: int):
-    x = x0
+    x: float = x0
+    title: str = 'f(x) = a sin(pi x)'
     for _ in range(n):
         x = a*math.sin(math.pi * x)
 
-    return x
+    return x, title
 
 
 def platformer():
